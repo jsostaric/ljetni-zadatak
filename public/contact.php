6851317d1bb1c27d8e793ef893be020ffb808296
Jurica Šoštarić <bugificiran@gmail.com>
2017-08-04 16:58:29 +0200

    stavljeni metapodaci i mapa u kontaktima

--- public/contact.php
+++ public/contact.php
@@ -14,14 +14,8 @@ include_once '../config.php';
 		<?php include_once '../templates/menu.php'; ?>
 		
 		<div class="row">
-			<div class="large-12 columns large-centered">
-				<h1 style="text-align: center;">TRUE SIGHT</h1>
-				<p style="text-align: center;">Characters and Adventures at your hand.</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="large-12 columns">
-				
+			<div class="large-12 columns large center">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44707.18063217925!2d18.65383953216996!3d45.54632644999991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475ce7a869728075%3A0x5b8c725621a41195!2sOsijek!5e0!3m2!1shr!2shr!4v1501858466741" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 			</div>
 		</div>
 		
--- templates/head.php
+++ templates/head.php
@@ -4,6 +4,9 @@
 
 <title><?php echo $title; ?></title>
 
+<meta property="og:title" content="True Sight" />
+<meta property="og:type" content="TTRPG" />
+
 
 <!-- favicon -->
 <link rel="icon" href="<?php echo $route; ?>img/favicon.ico" type="image/x-icon">
